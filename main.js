fdaf37154f52d6eef05cdffa0e624fa57dc8a548
Bartek Wieckowski <bart.it.pro@gmail.com>
2022-01-19 14:46:49 +0100

    added another if statement all options to win, high, low check number

--- main.js
+++ main.js
@@ -6,9 +6,23 @@ const attemptUser = document.querySelector(".attempt-user");
 const btnCheck = document.querySelector(".check");
 const btnAgain = document.querySelector(".again");
 
+const foundNumber = Math.trunc(Math.random() * 20) + 1;
+let attemptScore = 20;
+
+rightScore.textContent = foundNumber;
 btnCheck.addEventListener("click", function () {
   const guess = Number(attemptUser.value);
   if (!guess) {
     message.textContent = "⛔ No number!";
+  } else if (guess === foundNumber) {
+    message.textContent = "🎉 Correct Number";
+  } else if (guess > foundNumber) {
+    message.textContent = "📈 Too high!";
+    attemptScore--;
+    score.textContent = attemptScore;
+  } else {
+    message.textContent = "📉 Too low!";
+    attemptScore--;
+    score.textContent = attemptScore;
   }
 });
