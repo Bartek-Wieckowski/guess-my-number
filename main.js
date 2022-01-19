d410c9177bef0312261a7b4def6d61fb76f01348
Bartek Wieckowski <bart.it.pro@gmail.com>
2022-01-19 13:51:28 +0100

    few queryselectors

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+const message = document.querySelector(".heading-secondary");
+const rightScore = document.querySelector(".number");
+const score = document.querySelector(".score");
+const attemptUser = document.querySelector(".attempt-user");
+
+message.textContent = "Bart działa";
+rightScore.textContent = 13;
+score.textContent = 90;
+attemptUser.value = 30;
