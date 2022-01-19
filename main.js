f2d7e10dadcc17e78f06fc5f74cf4637f4cf1a4a
Bartek Wieckowski <bart.it.pro@gmail.com>
2022-01-19 14:16:32 +0100

    implementing function which check if user put a number

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const message = document.querySelector(".heading-secondary");
 const rightScore = document.querySelector(".number");
 const score = document.querySelector(".score");
 const attemptUser = document.querySelector(".attempt-user");
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
 
-message.textContent = "Bart działa";
-rightScore.textContent = 13;
-score.textContent = 90;
-attemptUser.value = 30;
+btnCheck.addEventListener("click", function () {
+  const guess = Number(attemptUser.value);
+  if (!guess) {
+    message.textContent = "⛔ No number!";
+  }
+});
